49898c3656d9e76780b42a96b16b70078c2cde4c
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-08-12 23:54:06 +0530

    atlast working

--- Assignment 2/DtreeGini.py
+++ Assignment 2/DtreeGini.py
@@ -1,29 +1,27 @@
-import sys
-import matplotlib
-matplotlib.use('Agg')
-
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
-import matplotlib.pyplot as plt
 
-df = pandas.read_csv("F:\Machine Learning Lab - 5th sem\Assignment 2\data.csv")
+# Read CSV
+df = pd.read_csv("F:\Machine Learning Lab - 5th sem\Assignment 2\data.csv")
 
+# Map categorical values
 d = {'UK': 0, 'USA': 1, 'N': 2}
 df['Nationality'] = df['Nationality'].map(d)
 d = {'YES': 1, 'NO': 0}
 df['Go'] = df['Go'].map(d)
 
+# Features and target
 features = ['Age', 'Experience', 'Rank', 'Nationality']
-
 X = df[features]
 y = df['Go']
 
+# Train Decision Tree
 dtree = DecisionTreeClassifier()
-dtree = dtree.fit(X, y)
+dtree.fit(X, y)
 
-tree.plot_tree(dtree, feature_names=features)
-
-#Two  lines to make our compiler able to draw:
-plt.savefig(sys.stdout.buffer)
-sys.stdout.flush()
+# Plot in Jupyter
+plt.figure(figsize=(10, 6))
+tree.plot_tree(dtree, feature_names=features, filled=True)
+plt.show()
